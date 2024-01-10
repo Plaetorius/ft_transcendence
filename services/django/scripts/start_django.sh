049afc760c6eb8273a8ff29b8e15deea84d42417
Tom GERNEZ <tgernez@student.42.fr>
2024-01-10 17:28:16 +0100

    add bootstrap

--- services/django/scripts/start_django.sh
+++ services/django/scripts/start_django.sh
@@ -2,9 +2,12 @@
 
 # Apply database migrations
 echo "Applying database migrations..."
-# python src/manage.py makemigrations # TODO maybe remove
+python src/manage.py makemigrations
 python src/manage.py migrate
 
 # Start server
 echo "Starting server..."
 python src/manage.py runserver 0.0.0.0:8000
+
+sleep 1
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('tgernez', 'tgernez@student.42.fr', 'tgernez')" | python manage.py shell
