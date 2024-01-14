e1331f17a4a434d8e79f17ac3e3c0582d6fb768d
Tom GERNEZ <tgernez@student.42.fr>
2024-01-14 18:04:12 +0100

    better friendship models

--- src/users/models.py
+++ src/users/models.py
@@ -9,27 +9,41 @@ class User(AbstractUser):
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True, default="profile_pictures/default.jpg")
     email = models.EmailField(unique=True, blank=False, null=False)
 
-# Match history storage
+# Match history Class
 class MatchHistory(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     game_type = models.CharField(max_length=100)
     score = models.IntegerField()
     date_played = models.DateTimeField(auto_now_add=True)
 
-# Chat Message storage
+# Chat Message Class
 class ChatMessage(models.Model):
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='sent_messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='received_messages', on_delete=models.CASCADE)
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
-# Friendship storage
+# Friendship Class
 class Friendship(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='friendships', on_delete=models.CASCADE)
     friend = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='friends', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ('user', 'friend')
 
-# Achievement Storage
+# Friend Request Class
+class FriendRequest(models.Model):
+    from_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="friend_request_sent", on_delete=models.CASCADE)
+    to_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="friend_request_received", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# Blocked User Class
+class BlockedUser(models.Model):
+    blocker = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='blocker', on_delete=models.CASCADE)
+    blocked = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='blocked_users', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+# Achievement Class
 class Achievement(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
