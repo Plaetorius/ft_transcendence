3cbec971d2d3d7524c2b9e2f5301332da897c3d1
Tom GERNEZ <tgernez@student.42.fr>
2024-01-15 12:40:51 +0100

    add FriendRequest to admin.py

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, MatchHistory, ChatMessage, Friendship, Achievement
+from .models import User, MatchHistory, ChatMessage, Friendship, Achievement, FriendRequest
 
 admin.site.register(User)
 admin.site.register(MatchHistory)
 admin.site.register(ChatMessage)
 admin.site.register(Friendship)
 admin.site.register(Achievement)
+admin.site.register(FriendRequest)
