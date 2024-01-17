4b07b0fa86e318db5abece38747a364a479ae199
Tom GERNEZ <tgernez@student.42.fr>
2024-01-17 15:44:40 +0100

    add pong app

--- src/transcendence/urls.py
+++ src/transcendence/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_view  # Import home_view from views.py
+from .views import home_view, games_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', home_view, name="home"),
+    path('pong/', include('pong.urls')),
     path('users/', include('users.urls')),
 ]
 
