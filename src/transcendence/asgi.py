711389de21f3119a6563dffc1ae4fe7d5d24b014
Tom GERNEZ <tgernez@student.42.fr>
2024-01-20 18:47:05 +0100

    add aynchronous chat

--- src/transcendence/asgi.py
+++ src/transcendence/asgi.py
@@ -2,16 +2,33 @@
 import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import path
 from users.consumers import FriendRequestConsumer
+from users.routing import websocket_urlpatterns
 from django.urls import re_path
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter([
-            re_path(r'^ws/friend-requests/(?P<username>\w+)/$', FriendRequestConsumer.as_asgi()),
-        ])
-    ),
-})
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "transcendence.settings")
+
+django_asgi_app = get_asgi_application()
+
+import users.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter([
+#             re_path(r'^ws/friend-requests/(?P<username>\w+)/$', FriendRequestConsumer.as_asgi()),
+#         ])
+#     ),
+# })
--- src/users/consumers.py
+++ src/users/consumers.py
@@ -30,3 +30,27 @@ class FriendRequestConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
        pass
         # Process incoming message
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"chat_{self.room_name}" # TODO Sanitize, no more than 100 char and alphanurmeric + hypens
+
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
+    
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat.message", "message": message}
+        )
+        # The special "type" key turns the . in _, thus "chat.message"
+        # corresponds to chat_message() (function above)
+
+    async def chat_message(self, event):
+        message = event["message"]
+        await self.send(text_data = json.dumps({"message": message}))
