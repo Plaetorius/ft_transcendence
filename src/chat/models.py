24640b9c4b987673247b74cbf941389a88ee0c4b
Tom GERNEZ <tgernez@student.42.fr>
2024-01-22 12:07:30 +0100

    add ChatRoom and Message classes

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,3 +1,16 @@
+# chat/models.py
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class ChatRoom(models.Model):
+    user1 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="user1", on_delete=models.CASCADE)
+    user2 = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="user2", on_delete=models.CASCADE)
+
+class Message(models.Model):
+    room = models.ForeignKey(ChatRoom, related_name="chatRoom", on_delete=models.CASCADE)
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="sender", on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
