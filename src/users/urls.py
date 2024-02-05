c4232f649968b7917425086c344f447e63bbe089
Tom GERNEZ <tgernez@student.42.fr>
2024-02-05 18:43:54 +0100

    clean old files

--- src/users/urls.py
+++ src/users/urls.py
@@ -15,7 +15,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('profile/<int:pk>/', UserProfileView.as_view(), name="user-profile"),
+    path('profile/<int:pk>/', UserProfileView.as_view(), name="user-profile-api"),
     path('register/', UserRegistrationAPIView.as_view(), name="register-api"),
 	path('login/', UserLoginAPIView.as_view(), name="login-api"),
 	path('search/<str:username>/', UserSearchAPIView.as_view(), name="search-api"),
