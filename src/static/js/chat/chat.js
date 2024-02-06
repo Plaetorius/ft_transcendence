54d48632f50e1f3ef0d732b1925cd9aeddddae43
Tom GERNEZ <tgernez@student.42.fr>
2024-02-06 16:59:31 +0100

    re-enable socket

--- src/static/js/chat/chat.js
+++ src/static/js/chat/chat.js
@@ -18,8 +18,8 @@ document.querySelectorAll(".chatRoomButton").forEach(element => {
             return response.json();
         })
         .then(data => {
-            let room_id = data.room_id;
-            console.log(`Room ID: ${room_id}`);
+            console.log(`Room ID: ${data.room_id}`);
+            enter_room(data.room_id)
         })
         .catch(error => {
             // TODO proper error handling
@@ -28,37 +28,37 @@ document.querySelectorAll(".chatRoomButton").forEach(element => {
     });
 });
 
-// let socket = null;
+let socket = null;
 
-// window.onload = () => {
-//     const username = 'example';
-//     socket = new WebSocket(`ws://${window.location.host}/ws/chat/${username}/`);
+function enter_room(room_id) {
+    socket = new WebSocket(`ws://${window.location.host}/ws/dm/${room_id}/`);
+    window.location.pathname = `/chat/dm/${room_id}/`;
+    
+    socket.onopen = (e) => {
+        console.log(`Web socket opened`);
+    };
 
-//     socket.onopen = (e) => {
-//         console.log(`Web socket opened`);
-//     };
+    socket.onmessage = (e) => {
+        const data = JSON.parse(e.data);
+        console.log(`Message rereceived: ${data.message}`);
+    };
 
-//     socket.onmessage = (e) => {
-//         const data = JSON.parse(e.data);
-//         console.log(`Message rereceived: ${data.message}`)
-//     }
-//     document.getElementById("send-message-btn").addEventListener('click', (e) => {
-//         e.preventDefault();
-//         let messageElem = document.getElementById('message-input');
-//         const message = messageElem.value;
-//         const username = 'example';
-//         messageElem.value = '';
-//         console.log(`Message: ${message}`);
+    document.getElementById("send-message-btn").addEventListener('click', (e) => {
+        e.preventDefault();
+        let messageElem = document.getElementById('message-input');
+        const message = messageElem.value;
+        messageElem.value = '';
+        console.log(`Message: ${message}`);
 
-//         if (socket.readyState === WebSocket.OPEN) {
-//             socket.send(JSON.stringify({
-//                 message: message,
-//             }));
-//             console.log(`Message sent: ${message}`);
-//         } else {
-//             console.error(`WebSocket is not open. State: ${socket.readyState}`);
-//         }
-//     })
-// }
+        if (socket.readyState === WebSocket.OPEN) {
+            socket.send(JSON.stringify({
+                message: message,
+            }));
+            console.log(`Message sent: ${message}`);
+        } else {
+            console.error(`WebSocket is not open. State: ${socket.readyState}`);
+        }
+    })
+}
 
 
