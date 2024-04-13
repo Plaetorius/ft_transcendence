b0643be460e70748996a7517770b9922f9e8d298
Tag6362 <tag6362@gmail.com>
2024-04-13 16:01:41 +0200

    fixed header profile picture

--- src/static/js/users/profile.js
+++ src/static/js/users/profile.js
@@ -27,6 +27,7 @@ function showProfile() {
 
 function loadMyProfile() {
     document.getElementById("my-profile-picture").src = user.profile_picture_url;
+	document.getElementById("header-profile-picture").src = user.profile_picture_url;
     document.getElementById("my-profile-username").innerHTML = `<span class="online-status online"></span>${user.username}`;
     document.getElementById("my-profile-elo").innerHTML = `<span>Elo: </span>${user.elo}`;
     const dateJoined = new Date(user.date_joined);
