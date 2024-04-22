c5199c75e5c21e51da99c838bbc5374205291951
Tom GERNEZ <tgernez@student.42.fr>
2024-04-22 23:54:10 +0200

    fix empty friend request

--- src/static/js/users/search.js
+++ src/static/js/users/search.js
@@ -1,16 +1,18 @@
 document.getElementById('userSearchForm').addEventListener('submit', async (e) => {
     e.preventDefault();
     const usernameInput = document.getElementById('searchUsername');
-    const username = usernameInput.value;
+    const username = usernameInput.value.trim();
 	usernameInput.value = '';
 
+	if (username.length === 0) {
+		notification("Username can't be empty!", "cross", "error");
+		return ;
+	}
+
 	addFriend(username).then(data => {
-        // Handle success, update the UI accordingly
 		notification(`Friend added: ${data.success}`, "check", "success");
 		actualiseFriendsSection();
     }).catch(error => {
-        // Log the backend error message if it exists, otherwise log a default error message
-        // Handle failure, perhaps show a message to the user
         notification(`Failed to add friend: ${error.error ? error.error : 'An error occurred'}`, "cross", "error");
     });
 	actualiseFriendsSection();
